5beaef2b945caf9964449481c19f0bb25d1fcf96
Pavel <zubarev.pol@gmail.com>
2022-07-14 12:30:58 +0200

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+"use strict"
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let open_brkt = new Map(bracketsConfig);
+  let close_brkt = new Map(bracketsConfig.map(a => a.reverse()));
+  let stack = [];
+
+  for (let char of str) {
+    //    console.log(`all: ${str}`);
+    //    console.log(`head: ${stack.join('')}`);
+    //    console.log(char);
+    // stack is empty
+    if (stack.length === 0) {
+      if (open_brkt.has(char)) {
+        stack.push(char);
+      } else {
+        return false;
+      }
+
+      // stack has value
+    } else {
+      // twean brackets
+      if (open_brkt.has(char) && close_brkt.has(char)) {
+        stack.at(-1) == char ? stack.pop() : stack.push(char);
+        // single bracket
+      } else if (open_brkt.has(char)) {
+        stack.push(char);
+      } else if (stack.at(-1) == close_brkt.get(char)) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+
+  }
+  return stack.length == 0 ? true : false;
 }
